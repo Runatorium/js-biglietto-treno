86ebc6b57d04de118b87da8c97388780a750cef1
Runatorium <stefano.alesci1@gmail.com>
2023-02-22 16:12:14 +0100

    final commit

--- js/main.js
+++ js/main.js
@@ -11,7 +11,23 @@ let costokmutente = (n_km * ppkm);
 
 console.log(costokmutente);
 
-if (eta_psg => over18){
-    console.log("L'utente è maggiorenne")
-
+if (eta_psg < over18){
+    console.log("L'utente è minorenne")
+    let prezzofinale = parseFloat((costokmutente * 80) /100).toFixed(2)
+    console.log("il costo finale è " + prezzofinale);   
+    document.getElementById("costofinale").innerHTML = `il prezzo del biglietto è: ${prezzofinale} €`;   
+   
+} else if (eta_psg >= over65){
+    console.log("L'utente è over65")
+    let prezzofinale = parseFloat((costokmutente * 60) /100).toFixed(2)
+    console.log("il costo finale è " + prezzofinale);
+    document.getElementById("costofinale").innerHTML = `il prezzo del biglietto è: ${prezzofinale} €`;  
+     
+} else {
+    console.log("l'utente non ha diritto a sconti");
+    let prezzofinale = parseFloat(costokmutente).toFixed(2)
+    document.getElementById("costofinale").innerHTML = `il prezzo del biglietto è: ${prezzofinale} €`;  
+       
 }
+
+
